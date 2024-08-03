abfdbc3c5feae27a05c25d7fbc914c139288bd7d
Cesar Rodriguez <cesarodriguez4@gmail.com>
2024-08-03 19:33:20 -0400

    Heroku origins included

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from chatgpt import router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    'https://chatgpt-experience-fe-55bc9e57d38a.herokuapp.com/',
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
